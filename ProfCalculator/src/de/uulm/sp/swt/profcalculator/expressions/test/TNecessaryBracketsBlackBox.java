bfb232c97468db7076a0f9b7e4c986c7eae51666
Tim Nolle <tn4799@gmail.com>
2021-09-15 15:33:47 +0200

    added more tests with combination of add and sub

--- ProfCalculator/src/de/uulm/sp/swt/profcalculator/expressions/test/TNecessaryBracketsBlackBox.java
+++ ProfCalculator/src/de/uulm/sp/swt/profcalculator/expressions/test/TNecessaryBracketsBlackBox.java
@@ -17,30 +17,36 @@ class TNecessaryBracketsBlackBox {
 	Expression add = new NecessaryBrackets(new Addition(value, value));
 	Expression sub = new NecessaryBrackets(new Sub(value, value));
 	Expression mult = new NecessaryBrackets(new Multiplication(value, value));
+	Expression addSub = new NecessaryBrackets(new Addition(sub, value)); // 1-1+1
 	
 	@Test
 	void testNoBrackets() {
 		assertEquals("1 + 1 * 1", new Addition(value, mult).toString());
 		assertEquals("1 - 1 * 1", new Sub(value, mult).toString());
-		assertEquals("1 - 1 + 1", new Sub(value, add).toString());
+		assertEquals("1 - 1 + 1 * 1", new Addition(sub, mult).toString());
 	}
 	
 	@Test
 	void testBracketsLeft() {
 		assertEquals("(1 + 1) * 1", new Multiplication(add, value).toString());
 		assertEquals("(1 - 1) * 1", new Multiplication(sub, value).toString());
+		assertEquals("(1 - 1 + 1) * 1", new Multiplication(addSub, value).toString());
 	}
 	
 	@Test
 	void testBracketsRight() {
 		assertEquals("1 * (1 + 1)", new Multiplication(value, add).toString());
 		assertEquals("1 * (1 - 1)", new Multiplication(value, sub).toString());
+		assertEquals("1 * (1 - 1 + 1)", new Multiplication(value, addSub).toString());
 	}
 	
 	@Test
 	void testBracketsLeftAndRight() {
 		assertEquals("(1 + 1) * (1 + 1)", new Multiplication(add, add).toString());
 		assertEquals("(1 - 1) * (1 - 1)", new Multiplication(sub, sub).toString());
+		assertEquals("(1 - 1 + 1) * (1 - 1)", new Multiplication(addSub, sub).toString());
+		assertEquals("(1 - 1) * (1 - 1 + 1)", new Multiplication(sub, addSub).toString());
+		assertEquals("(1 - 1 + 1) * (1 - 1 + 1)", new Multiplication(addSub, addSub).toString());
 	}
 
 }
